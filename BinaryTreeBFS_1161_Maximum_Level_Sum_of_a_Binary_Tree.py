1cc300ec36bb25ac0295f229c794e51647c46c8a
konarkyadav <k.yadav2704@gmail.com>
2023-12-23 02:11:56 -0500

    Add files via upload

---
+++ BinaryTreeBFS_1161_Maximum_Level_Sum_of_a_Binary_Tree.py
@@ -0,0 +1,34 @@
+# Reference: https://www.youtube.com/watch?v=7CgPNyJZ0xM&ab_channel=ErraK
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return
+        queue = [root]
+        max_level = 1
+        level = 1
+        max_sum = float('-inf')
+
+        while queue:
+            level_sum = 0
+            next_level = []
+            for node in queue:
+                level_sum += node.val
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = level
+            
+            queue = next_level
+            level += 1
+        return max_level
