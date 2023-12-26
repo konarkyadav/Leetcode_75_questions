1989e7cf0d1412c53f74c3517e97c4f492402cfb
konarkyadav <k.yadav2704@gmail.com>
2023-12-25 19:48:24 -0500

    Add files via upload

---
+++ Heap_Priority_Queue_2542_Maximum_Subsequence_Score.py
@@ -0,0 +1,19 @@
+# Reference: https://www.youtube.com/watch?v=ax1DKi5lJwk&t=263s&ab_channel=NeetCodeIO
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        pairs = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        pairs = sorted(pairs, key=lambda p: p[1], reverse = True)
+        minHeap = []
+        n1Sum = 0
+        res = 0
+        for n1, n2 in pairs:
+            n1Sum += n1
+            heapq.heappush(minHeap, n1)
+
+            if len(minHeap) > k:
+                n1Pop = heapq.heappop(minHeap)
+                n1Sum -= n1Pop
+            if len(minHeap) == k:
+                res = max(res, n2*n1Sum)
+        return res
+
