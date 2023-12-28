45242d6fc856c8c91b1590c6be6ef1c99b719519
Konark Yadav <k.yadav2704@gmail.com>
2023-12-28 12:00:05 -0500

    Add files via upload

---
+++ BackTracking_17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,24 @@
+# Reference: https://www.youtube.com/watch?v=0snEunUacZY&ab_channel=NeetCode
+# Reference: https://www.youtube.com/watch?v=tWnHbSHwNmA&ab_channel=CodeHelp-byBabbar
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        dictionary = {"2": "abc",
+                      "3": "def",
+                      "4": "ghi",
+                      "5": "jkl",
+                      "6": "mno",
+                      "7": "pqrs",
+                      "8": "tuv",
+                      "9": "wxyz"}
+        
+        def helper(i, currstr):
+            if (len(currstr) == len(digits)):
+                res.append(currstr)
+                return
+            for c in dictionary[digits[i]]:
+                helper(i+1, currstr+c)
+        if digits:
+            helper(0, "")
+        return res
