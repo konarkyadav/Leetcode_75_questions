e0cb80951c733a6ceda5b3af52c103027716126c
Konark Yadav <k.yadav2704@gmail.com>
2024-01-04 13:58:46 -0500

    Update Intervals_452_Minimum_Number_of_Arrows_to_Burst_Balloons.py
    New code added

--- Intervals_452_Minimum_Number_of_Arrows_to_Burst_Balloons.py
+++ Intervals_452_Minimum_Number_of_Arrows_to_Burst_Balloons.py
@@ -1,14 +1,26 @@
 # Reference: https://www.youtube.com/watch?v=_WIFehFkkig&ab_channel=TimothyHChang
+# https://walkccc.me/LeetCode/problems/0452/#__tabbed_1_3
+# class Solution:
+#     def findMinArrowShots(self, points: List[List[int]]) -> int:
+#         points.sort()
+#         prev = points[0]
+#         total = 1
+#         for s,e in points[1:]:
+#             if s > prev[1]:
+#                 total += 1
+#                 prev = [s,e]
+#             else:
+#                 prev[1] = min(prev[1], e)
+#         return total
 
 class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()
-        prev = points[0]
-        total = 1
-        for s,e in points[1:]:
-            if s > prev[1]:
-                total += 1
-                prev = [s,e]
-            else:
-                prev[1] = min(prev[1], e)
-        return total
+  def findMinArrowShots(self, points: List[List[int]]) -> int:
+    ans = 0
+    arrowX = -math.inf
+
+    for point in sorted(points, key=lambda x: x[1]):
+      if point[0] > arrowX:
+        ans += 1
+        arrowX = point[1]
+
+    return ans
