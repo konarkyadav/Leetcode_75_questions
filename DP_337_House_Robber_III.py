76c34553d603b8c269c61a8a2362a119dcae0d1e
Konark Yadav <k.yadav2704@gmail.com>
2024-01-05 15:09:31 -0500

    Add files via upload

---
+++ DP_337_House_Robber_III.py
@@ -0,0 +1,24 @@
+# Reference: https://www.youtube.com/watch?v=nHR8ytpzz7c&t=1s&ab_channel=NeetCode
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        
+        # Return pair [withRoot, wothoutRoot] 
+        def dfs(root):
+            if not root:
+                return [0,0]
+            leftPair = dfs(root.left)
+            rightPair = dfs(root.right)
+
+            withRoot = root.val + leftPair[1] + rightPair[1]
+            withoutRoot = max(leftPair) + max(rightPair)
+
+            return [withRoot, withoutRoot]
+        
+        return max(dfs(root))
